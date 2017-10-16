1ae8f4defe3252fbbbcd7bfd25a7ff724e053aab
Jock Dalby <jockdalby86@gmail.com>
2017-10-16 17:43:39 +1300

    add ajax request example

--- src/app/components/components.ts
+++ src/app/components/components.ts
@@ -4,11 +4,13 @@ import { FromMethodShortComponent } from './from-method-short.component';
 import { CreateMethodComponent } from './create-method.component';
 import { AsyncCreateMapFilterComponent } from './async-create-map-filter.component';
 import { MovingCircleComponent } from './moving-circle.component';
+import { GetMoviesComponent } from './get-movies.component';
 
 export const COMPONENTS = [
     CreateMethodComponent,
     AsyncCreateMapFilterComponent,
     FromMethodComponent,
     FromMethodShortComponent,
+    GetMoviesComponent,
     MovingCircleComponent
 ];
